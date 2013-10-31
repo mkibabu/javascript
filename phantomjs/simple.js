96c01acc1293d855eddd9607a33cd2bb2d0fb404
Michael Muraya <mkibabu@gmail.com>
2013-10-31 13:42:41 -0400

    Add simple.js
    Source: net.tutsplus.com

---
+++ phantomjs/simple.js
@@ -0,0 +1,11 @@
+// Source: net.tutsplus
+
+console.log("We can log stuff out too");
+function add (a, b)
+{
+    return a + b;
+}
+
+console.log("We can execute regular js too:", add(4, 7));
+
+phantom.exit();
